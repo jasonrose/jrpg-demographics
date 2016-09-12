87409a0890202e4bfd55bc7ac69fbabae7504ec5
Jason Rose <jrose@shutterfly.com>
2016-09-11 22:09:22 -0700

    Adding initial data

---
+++ data.js
@@ -0,0 +1,4 @@
+var requireDirectory = require('require-directory');
+var data = requireDirectory(module, './src/data');
+exports.data = data;
+///////////////////////////////////////////////////////////////////////////////
